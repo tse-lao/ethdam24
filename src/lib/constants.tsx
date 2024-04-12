46fcde92a2886230c80a62e4231502552581754c
Koen Schuite <106653359+tse-lao@users.noreply.github.com>
2024-04-13 07:28:57 +0800

    Update constants.tsx

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -1,2 +1,2 @@
-export const API_URL = process.env.PUBLIC_API || "http://localhost:4000";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
 export const DEFAULT_AVATAR = "/images/default.png";
